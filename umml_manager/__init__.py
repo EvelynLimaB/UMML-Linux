d874b9fe97090746f0f20a11a487732a57cc5534
Evelyn Lima <104331636+EvelynLimaB@users.noreply.github.com>
2026-07-22 23:06:04 -0300

    Expose serialized manager library as public API

--- umml_manager/__init__.py
+++ umml_manager/__init__.py
@@ -1,9 +1,9 @@
 """UMML Manager: deterministic profiles and transactional asset deployment."""
 
 from .deployment import ApplyEngine, ApplyError
+from .library import ManagerStore
 from .models import ModRecord, Profile
 from .resolver import Resolution, resolve_profile
-from .store import ManagerStore
 
 __all__ = [
     "ApplyEngine",
